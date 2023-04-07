4a5f7768ce2235fdf404099cc0af046b5c2db7a9
cfgm1603 <cesargimo@unisabana.edu.co>
2023-04-07 14:56:29 -0500

    Prueba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import BlogList from './BlogList.js';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Bienvenidos
+      <br></br>
+      <button onClick={
+        () => {
+          
+          window.location.href = '/BlogList.js';
+        }
+      }>BlogList</button>
     </div>
   );
 }
---
+++ src/BlogList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function BlogList() {
+    return (
+        <div className="BlogList">
+        <h1>BlogList</h1>
+        <Link to="/BlogList.js">BlogList</Link>
+
+
+        </div>
+    );
+}
+
+
