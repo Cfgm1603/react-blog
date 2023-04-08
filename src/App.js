1ccc0faeeeb76a5a86e3600e87b789c6d1d30f22
cfgm1603 <cesargimo@unisabana.edu.co>
2023-04-07 19:22:57 -0500

    Ya nos podemos mover entre paginas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import BlogList from './BlogList.js';
+
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+
+//Navegación entre las paginas
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+
+//Para API
+import React, { useEffect, useState } from 'react';
+//f108dff7dcmsh5d9ea543da5bce3p1ba432jsnd8f1dd25cd03
+
+
+//Importamos las paginas que vamos a utilizar
+import Layout from './layout';
+import Home from './home';
+import BlogList from './components/BlogList';
 
 function App() {
   return (
-    <div className="App">
-      Bienvenidos
-      <br></br>
-      <button onClick={
-        () => {
-          
-          window.location.href = '/BlogList.js';
-        }
-      }>BlogList</button>
-    </div>
+    //Esto va a direccionar a la ruta que se le indique 
+    <Router>
+    <Routes>
+      //Layout le da la estructura base a todas las paginas
+      <Route path="/" element={<Layout />}>
+        //Ahora se indica que la ruta inicial va a ser la pagina Home
+        <Route index element={<Home />} />
+        //Ahora se indica que el tag bloglist va a ser la pagina BlogList
+        <Route path="bloglist" element={<BlogList />} />
+      </Route>
+    </Routes>
+  </Router>
   );
 }
 
